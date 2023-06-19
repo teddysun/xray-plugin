1636ccf13e7bb201ee37e08257c6b60c11f1f1e2
Teddysun <i@teddysun.com>
2023-06-19 13:33:21 +0900

    Update comments Fixed a issue when build Android APP
    Signed-off-by: Teddysun <i@teddysun.com>

--- utils_android.go
+++ utils_android.go
@@ -73,39 +73,36 @@ import (
 	vinternet "github.com/xtls/xray-core/transport/internet"
 )
 
-func ControlOnConnSetup(network string, address string, s uintptr) error {
-	fd := int(s)
-	path := "protect_path"
+func ControlOnConnSetup(network string, address string, conn syscall.RawConn) error {
+	return conn.Control(func(s uintptr) {
+		fd := int(s)
+		path := "protect_path"
 
-	socket, err := syscall.Socket(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+		socket, err := syscall.Socket(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
+		if err != nil {
+			log.Println(err)
+		}
 
-	defer syscall.Close(socket)
+		defer syscall.Close(socket)
 
-	C.set_timeout(C.int(socket))
+		C.set_timeout(C.int(socket))
 
-	err = syscall.Connect(socket, &syscall.SockaddrUnix{Name: path})
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+		err = syscall.Connect(socket, &syscall.SockaddrUnix{Name: path})
+		if err != nil {
+			log.Println(err)
+		}
 
-	C.ancil_send_fd(C.int(socket), C.int(fd))
+		C.ancil_send_fd(C.int(socket), C.int(fd))
 
-	dummy := []byte{1}
-	n, err := syscall.Read(socket, dummy)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	if n != 1 {
-		log.Println("Failed to protect fd: ", fd)
-	}
-
-	return nil
+		dummy := []byte{1}
+		n, err := syscall.Read(socket, dummy)
+		if err != nil {
+			log.Println(err)
+		}
+		if n != 1 {
+			log.Println("Failed to protect fd: ", fd)
+		}
+	})
 }
 
 func registerControlFunc() {
